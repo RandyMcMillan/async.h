ed3f3e732a6a9cd73fb000ea25a313f8939ff47a
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-11-20 08:26:48 -0500

    async/example_state.h

--- async/example_state.h
+++ async/example_state.h
@@ -1,3 +1,6 @@
+//#ifndef __EXAMPLESTATE_H__
+//#define __EXAMPLESTATE_H__
+
 #include "async.h"
 
 #include <stdio.h>
@@ -12,7 +15,7 @@ typedef struct {
 // Store the formatted string of time in the output
 void format_time(char *output){
     
-    printf("format_time:\n");
+    printf("format_time: ");
 
     time_t rawtime;
     struct tm * timeinfo;
@@ -28,29 +31,21 @@ void format_time(char *output){
 async nested(struct async *pt3){
 
     async_begin(pt3);
-    printf("nested:async_begin(pt);\n");
+    //printf("nested: async_begin(pt);\n");
 
     time_t rawtime;
     struct tm * timeinfo;
     time ( &rawtime );
     timeinfo = localtime ( &rawtime );
-    char output = timeinfo->tm_sec;
-    printf ( "Current local time and date: %s", asctime (timeinfo) );
-    format_time(&output);
+    printf ( "nested: Current local time and date: %s", asctime (timeinfo) );
+    printf("format_time: ");
+    printf("[%d %d %d %d:%d:%d]\n", timeinfo->tm_mday,
+            timeinfo->tm_mon + 1, timeinfo->tm_year + 1900,
+            timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 
-/*
- The precision given after the . specifies the maximum number
- of characters to output from the string.
- It can be given as a decimal number or as *
- and provided as an int argument before the char pointer.
-*/
-
-    printf("value of a_static: %.40s\n", &output);
-    printf("value of b_static: %.*s\n", (int)sizeof(output), &output);
-
-    
-    printf("nested:async_end;\n");
     async_end;
 
 }
 state pt3;
+
+//#endif
