9397deb46aef06a8196643e0b86832c6c40b00be
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-11-20 08:28:07 -0500

    async/main.c

--- async/main.c
+++ async/main.c
@@ -1,9 +1,8 @@
-
 #include "examples.h"
 
 int main(void) {
-	example_small(200);
-	//example_buffer();
-	//example_codelock();
+	example_small(100);
+	example_buffer();
+	example_codelock();
 	return 0;
 }
